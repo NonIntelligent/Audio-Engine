ee724ae53c46c4d7206bb62582d6df6d12178a52
Amaan Miah <amaanmiah10@gmail.com>
2020-07-10 16:27:53 +0100

    Main files

---
+++ Audio Engine Git/Audio Engine/src/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+
+bool initGLFW() {
+	return true;
+}
+
+bool initGLEW() {
+	return true;
+}
+
+bool initALL() {
+	return true;
+}
+
+int main() {
+
+	std::cout << "Hola" << std::endl;
+
+	return 0;
+}
